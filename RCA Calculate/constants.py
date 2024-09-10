5ab47af8271049e0796bf45545ecc777b0d76dce
Annedrew <1196244890@qq.com>
2024-09-10 09:57:52 +0200

    Update constants.

--- RCA Calculate/constants.py
+++ RCA Calculate/constants.py
@@ -1,4 +1,11 @@
 import os
-FILE_PATH = f"{os.getcwd()}/BACI_HS12_V202401b"
+
+FOLDER_PATH = f"{os.getcwd()}/RCA Calculate/BACI_HS12_V202401b"
+
+COUNTRY_FILE = f"{FOLDER_PATH}/country_codes_V202401b.csv"
+
+PRODUCT_FILE = f"{FOLDER_PATH}/product_codes_HS12_V202401b.csv"
+
 PROD = [121221, 121229]
+
 VAL = ['v', 'q']
