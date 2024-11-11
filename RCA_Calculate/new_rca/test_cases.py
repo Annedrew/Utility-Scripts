e3330627283a3c80b0a86130109cedea4f251f5c
Annedrew <1196244890@qq.com>
2024-11-11 17:38:35 +0100

    Add test file and minor change.

--- RCA_Calculate/new_rca/test_cases.py
+++ RCA_Calculate/new_rca/test_cases.py
@@ -4,48 +4,52 @@ from rca_utility import *
 from constants import *
 
 
-df_test = pd.DataFrame({
-    'j': ['A', 'A', 'B', 'B'],  # importer
-    'i': ['C', 'C', 'D', 'D'],  # exporter
-    'v': [100, 150, 200, 250],
-    'q': [10, 15, 20, 25]
-})
-
-
-def test_all_imp(df_test):
+def test_all_imp():
     rca = RCA()
-    result_single = rca.all_imp(df_test, 'v', 'A', 'C')
-    assert result_single == 250, f"Expected 250 but got {result_single}"
+    df_test = pd.read_csv("RCA_Calculate/BACI_HS12_V202401b_test/BACI_HS12_Y2023_V202401b_test.csv")
 
-    result_all_exporters = rca.all_imp(df_test, 'v', 'A', 'all')
-    assert result_all_exporters == 250, f"Expected 250 but got {result_all_exporters}"
+    result_single = rca.all_imp(df_test, 'v', 8, 4)
+    assert result_single == 2.844, f"Expected 2.844 but got {result_single}"
 
-    result_all_exporters_d = rca.all_imp(df_test, 'v', 'B', 'all')
-    assert result_all_exporters_d == 450, f"Expected 450 but got {result_all_exporters_d}"
+    result_all_exporters = rca.all_imp(df_test, 'v', 32, 'all')
+    assert result_all_exporters == 1.8970000000000002, f"Expected 1.8970000000000002 but got {result_all_exporters}"
+
+    result_all_exporters_q = rca.all_imp(df_test, 'q', 12, 'all')
+    assert result_all_exporters_q == 1.2, f"Expected 1.2 but got {result_all_exporters_q}"
+
+    result_all_exporters_q2 = rca.all_imp(df_test, 'q', 32, 'all')
+    assert result_all_exporters_q2 == 0.172, f"Expected 0.172 but got {result_all_exporters_q2}"
 
 
-def test_generate_xwj(df_test):
+def test_generate_xwj():
     rca = RCA()
-    result_all = rca.generate_xwj("RCA_Calculate/BACI_HS12_V202401b", "BACI_HS12_Y2023_V202401b_test.csv", ['v', 'q'], "all")
-    
+    result_all = rca.generate_xwj("RCA_Calculate/BACI_HS12_V202401b_test", "BACI_HS12_Y2023_V202401b_test.csv", ['v', 'q'], "all")
+
     expected_all = [
         ['2023', np.int64(4), np.int64(8), 2.844, 0.126], 
         ['2023', np.int64(4), np.int64(12), 1.185, 1.2], 
-        ['2023', np.int64(4), np.int64(32), 1.8970000000000002, 0.172]]
+        ['2023', np.int64(4), np.int64(32), 1.8970000000000002, 0.172], 
+        ['2023', np.int64(4), np.int64(20), 0.0, 0.0], 
+        ['2023', np.int64(8), np.int64(8), 0.0, 0.0], 
+        ['2023', np.int64(8), np.int64(12), 0.0, 0.0], 
+        ['2023', np.int64(8), np.int64(32), 0.0, 0.0], 
+        ['2023', np.int64(8), np.int64(20), 0.787, 0.19]
+    ]
     
     assert result_all == expected_all, f"Expected {expected_all} but got {result_all}"
     
-    result_selected = rca.generate_xwj("RCA_Calculate/BACI_HS12_V202401b", "BACI_HS12_Y2023_V202401b_test.csv", ['v', 'q'], COUNTRY_CODE)
+    result_selected = rca.generate_xwj("RCA_Calculate/BACI_HS12_V202401b_test", "BACI_HS12_Y2023_V202401b_test.csv", ['v', 'q'], COUNTRY_CODE)
     
     expected_selected = [
         ['2023', 4, 4, 0.0, 0.0], 
         ['2023', 4, 8, 2.844, 0.126], 
         ['2023', 8, 4, 0.0, 0.0], 
-        ['2023', 8, 8, 0.0, 0.0]]
+        ['2023', 8, 8, 0.0, 0.0]
+    ]
 
     assert result_selected == expected_selected, f"Expected {expected_selected} but got {result_selected}"
 
 
-test_generate_xwj(df_test)
-# test_all_imp(df_test)
+test_all_imp()
+test_generate_xwj()
 print("All tests passed!")
